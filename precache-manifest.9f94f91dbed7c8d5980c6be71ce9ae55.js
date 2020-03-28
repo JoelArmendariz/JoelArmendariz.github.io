92ee50509241d1c581c82e60c64fbf33a699e303
Joel Armendariz <joelarmendariz@Joels-MBP.attlocal.net>
2020-03-28 00:32:35 -0500

    Updates

---
+++ precache-manifest.9f94f91dbed7c8d5980c6be71ce9ae55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987b58b77c58a764d1577c04a4699f23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202eb7fed76e78f9799a",
+    "url": "/static/css/main.355abd18.chunk.css"
+  },
+  {
+    "revision": "51260b987600c9183e14",
+    "url": "/static/js/2.418f376c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.418f376c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202eb7fed76e78f9799a",
+    "url": "/static/js/main.537a7701.chunk.js"
+  },
+  {
+    "revision": "f802db7f6b67856c54ea",
+    "url": "/static/js/runtime-main.c9bd7ec9.js"
+  },
+  {
+    "revision": "93a8ac1424a4bc9b1f310e1625c95336",
+    "url": "/static/media/JoelHeadshotFull.93a8ac14.jpg"
+  },
+  {
+    "revision": "973f67b17c18b4f53436a903623e595a",
+    "url": "/static/media/home-studio-desk.973f67b1.jpg"
+  }
+]);
